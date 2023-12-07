0e7aa3d0ad0864f6ff4ecdabe28ac6ce0f38e541
Zachary Waweru <azwdevops@gmail.com>
2023-12-07 13:42:48 +0300

    changed instances from max to 1 in ecosystem config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "carhub",
       script: "node_modules/.bin/next", // Adjust the path to the Next.js binary if needed
       args: "start",
-      instances: "max",
+      instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
