a7cef85a9a230e2707a7f8b97d2ff354f48d647f
Zachary Waweru <azwdevops@gmail.com>
2023-12-07 08:06:23 +0300

    car hub web app for show casing cars, created with nextjs, rapidapi(for car data) and imagin.studio(for car images)

---
+++ app/loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <main className="text-center h-[80vh] p-[30vh]">
+      <h2 className="text-purple-800 font-bold text-2xl mb-3">Loading...</h2>
+      <p className="text-blue-400 font-bold text-xl">
+        Hopefully not for too long :)
+      </p>
+    </main>
+  );
+};
+
+export default Loading;
